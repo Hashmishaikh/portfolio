b3d8c3975155bc9107795d1d6e62d20e9d3cde89
Hashmi Shaikh <hashmi.shaikh@mexcelle.com>
2026-04-19 11:52:19 +0530

    feat: add fallback route for SPA routing and update gitignore to exclude frontend build artifacts

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,11 @@ app.get("/", (req, res) => {
 
 app.use("/api/contactus", contactusRoutes);
 
+// This must be last, after all other routes
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+});
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
